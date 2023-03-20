0fab36d9dcd7515f1fd77e891619361aa1105135
Huu Khai NGUYEN (luciffer1904) <khainguyenhuu121@gmail.com>
2023-03-20 15:55:49 +0100

    Test travis

--- front/src/App.js
+++ front/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Change file to test travis-ci
+          Learn more
         </a>
       </header>
     </div>
